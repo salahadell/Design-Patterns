4038641e3b1b57502afda86cd888653a883a7a42
Salah <salah-adel2011@hotmail.com>
2024-09-27 05:51:21 +0300

    just changed the name of customer class to user to be able to see the difference before and after using observer DP

--- ObserverDesignPattern/user.cpp
+++ ObserverDesignPattern/user.cpp
@@ -1,43 +1,10 @@
-#include "user.h"
-
-user::user(std::string name, bool isSubscribedToProducts,
-	bool isSubscribedToOffers) 
+#include "customer.h"
+customer::customer(std::string name)
 {
 	this->name = name;
-	this->isSubscribedToProducts = isSubscribedToProducts;
-	this->isSubscribedToOffers = isSubscribedToOffers;
 
 }
-bool user::isSubscribedToProduct() 
+void customer::notify(std::string message)
 {
-	return this->isSubscribedToProducts;
-}
-bool user::isSubscribedToOffer() 
-{
-	return this->isSubscribedToOffers;
-}
-void user::subscribeToProducts() 
-{
-	this->isSubscribedToProducts = true;
-}
-void user::unsubscribeToProducts()
-{
-	this->isSubscribedToProducts = false;
-}
-void user::subscribeToOffers()
-{
-	this->isSubscribedToOffers = true;
-}
-void user::unsubscribeToOffers()
-{
-	this->isSubscribedToOffers = false;
-}
-void user::notifyuser(product* product) 
-{
-	std::cout << "user " + name + " notified that product " + product->getProductName() + " is added" <<std::endl;
-}
-void user::notifyuser(offer* offer)
-{
-	std::cout << "user " + name + " notified that " + offer->getMessage() << std::endl;
-
+	std::cout << "customer " + name + " notified that " + message << std::endl;
 }
--- ObserverDesignPattern/user.h
+++ ObserverDesignPattern/user.h
@@ -1,24 +1,15 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include "offer.h"
-#include "product.h"
-class user
+#include "subscriber.h"
+
+
+class customer:public subscriber
 {
 	std::string name;
-	bool isSubscribedToProducts;
-	bool isSubscribedToOffers;
 public:
-	user(std::string name, bool isSubscribedToProducts,
-		bool isSubscribedToOffers);
-	void subscribeToProducts();
-	void subscribeToOffers();
-	void unsubscribeToProducts();
-	void unsubscribeToOffers();
-	bool isSubscribedToProduct();
-	bool isSubscribedToOffer();
-	void notifyuser(product* product);
-	void notifyuser(offer* offer);
+	customer(std::string name);
+	void notify(std::string);
 
 };
 
